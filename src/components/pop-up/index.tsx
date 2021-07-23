db99fabbba277ad3f84612946392f15ba946d745
KiryaFT <gusev.kirill@mywish.io>
2021-07-23 12:35:12 +0300

    Fix popup

--- src/components/pop-up/index.tsx
+++ src/components/pop-up/index.tsx
@@ -1,18 +1,18 @@
 import { FunctionalComponent, h } from 'preact';
 
 import style from './style.scss';
-import {useState} from "preact/hooks";
+import { useState } from 'preact/hooks';
 
 const PopUp: FunctionalComponent = () => {
   const [close, setClose] = useState(false);
   if (!close) return (
     <div className={style.popup}>
       <button className={style.btn__close} onClick={() => setClose(true)}>
-        <img src="../../assets/img/icons/icon-close.svg" alt="icon close"/>
+        <img src="../../assets/img/icons/icon-close.svg" alt="icon close" />
       </button>
       <p>
         We use cookies to ensure that we give you the best experience on our website.
-        <a href="#"> Privacy policy</a>
+        <a href="/privacy-policy" target='_top'> Privacy policy</a>
       </p>
       <button className={style.btn__accept} onClick={() => setClose(true)}>Accept</button>
     </div>
