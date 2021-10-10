7d71dbd5a20a9c5c060f1c44eb683d33694002ca
Daniil Tsybenko <rusm9snik2013@yandex.ru>
2021-10-10 22:22:10 +0300

    little fixes

--- src/components/key-areas/index.tsx
+++ src/components/key-areas/index.tsx
@@ -4,7 +4,7 @@ import style from './style.scss';
 const KeyAreas: FunctionalComponent = () => {
     return (
         <div className={style['key-areas']}>
-            <h2>Key areas <span>of our work</span></h2>
+            <h2>Key areas of <span>our work</span></h2>
             <img className={style['key-areas_img']} src='../../assets/img/10knft/10knft-key-areas.svg' alt='key areas of our work' />
             <img className={style['key-areas_bg']} src='../../assets/img/10knft/key-areas-bg.svg' alt='bg' />
         </div>
