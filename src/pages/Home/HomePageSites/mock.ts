4e1be5214a508c147239a61d73dd432ea3ed84fe
krok101g <krok101g@gmail.com>
2021-10-18 20:58:36 +0400

    bug fix

--- src/pages/Home/HomePageSites/mock.ts
+++ src/pages/Home/HomePageSites/mock.ts
@@ -1,33 +1,27 @@
-import { SiteI } from '../../../types';
-import image1 from '../../../assets/img/farming/icon-site1.png';
-import image2 from '../../../assets/img/farming/icon-site2.png';
-import image3 from '../../../assets/img/farming/icon-site3.png';
-import image4 from '../../../assets/img/farming/icon-site4.png';
-import image5 from '../../../assets/img/farming/icon-site5.png';
+interface SiteI {
+    image: string;
+    title: string;
+    link: string;
+}
 
 export const otherSites: SiteI[] = [
     {
-        image: image1,
-        title: 'NFT Marketplace',
-        link: 'http://nftmarketplace.rocknblock.io/'
-    },
-    {
-        image: image2,
+        image: '../../../assets/img/farming/icon-site2.png',
         title: 'Farming',
         link: ''
     },
     {
-        image: image3,
+        image: '../../../assets/img/farming/icon-site3.png',
         title: '10k nft generator',
         link: 'http://10knft.rocknblock.io/'
     },
     {
-        image: image4,
+        image: '../../../assets/img/farming/icon-site4.png',
         title: 'Crowdsale',
         link: 'http://crowdsale.rocknblock.io/'
     },
     {
-        image: image5,
+        image: '../../../assets/img/farming/icon-site5.png',
         title: 'Vesting',
         link: ''
     }
